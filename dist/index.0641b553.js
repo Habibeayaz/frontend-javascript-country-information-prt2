f02b1f1a5800e96f54d16dbcebcb6309e296876c
Habibeayaz <habibe_ayaz@hotmail.com>
2023-03-08 12:14:51 +0100

    uitwerkingen

--- dist/index.0641b553.js
+++ dist/index.0641b553.js
@@ -575,18 +575,40 @@ async function fetchData(name) {
         const result = await (0, _axiosDefault.default).get(`https://restcountries.com/v2/name/${name}`);
         const country = result.data[0];
         console.log(result.data[0]);
-        searchResult.innerHTML = `
-        <h3>${country.name}</h3>
-        <p>${country.region} ${currencyCreator(country.currencies)}</p>    `;
+        searchResult.innerHTML = `<img class="flag" src="${country.flag}" alt="The national flag of ${country.name}" id="country-flag" flag" width="50">
+              <h2>${country.name}</h2>
+              <p>${country.name} is situated in ${country.region}. It has a population of ${country.population} people</p>
+              <p>The capital is ${country.capital} ${currencyCreator(country.currencies)}</p>
+              <!--<p>{countryLanguage}</p>-->
+              `;
     } catch (e) {
         console.error(e);
+        errorMessageBox.innerHTML = `
+      <p class="error-message">${name} not found. Please try again.</p>
+    `;
     }
 }
 function currencyCreator(currencies) {
-    let output = " and you can pay with";
+    let output = " and you can pay with ";
     if (currencies.length === 2) return output + `${currencies[0].name} and ${currencies[1].name}'s`;
     else return output + `${currencies[0].name}`;
-}
+} /*
+function countryLanguage(language){
+
+    let languageString = '';
+
+    switch (numLanguages) {
+        case 1:
+            languageString = `They speak ${languages[0]}`;
+            break;
+        case 2:
+            languageString = `They speak ${languages[0]} and ${languages[1]}`;
+            break;
+        default:
+            languageString = `They speak ${languages.slice(0, -1).join(', ')} and ${languages[numLanguages - 1]}`;
+            break;
+    }
+}*/ 
 
 },{"axios":"jo6P5","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"jo6P5":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
